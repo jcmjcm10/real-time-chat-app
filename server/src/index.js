f59c410be534c924f5e0e0530729695a01b81846
Jaume Cané <39407223+jcmjcm10@users.noreply.github.com>
2026-04-27 14:38:48 +0200

    Project init + docker compose

---
+++ server/src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+// const connectMongo = require('./config/mongo');
+// const connectRedis = require('./config/redis');
+
+// const authRoutes = require('./routes/auth');
+// const roomRoutes = require('./routes/rooms');
+// const messageRoutes = require('./routes/messages');
+
+// const registerSocketHandlers = require('./socket');
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: '*' }
+});
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/auth', authRoutes);
+// app.use('/api/rooms', roomRoutes);
+// app.use('/api/rooms', messageRoutes);
+
+// registerSocketHandlers(io);
+
+const PORT = process.env.PORT || 3000;
+
+async function start() {
+  // await connectMongo();
+  // await connectRedis();
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+start();
